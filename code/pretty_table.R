843bfc858fdfa2680d53e6053f27f5982cd57677
Cabous Olivier <cabousolivier14@gmail.com>
2022-11-26 15:41:02 +0200

    table

---
+++ code/pretty_table.R
@@ -0,0 +1,13 @@
+pretty_table <- function(df) {
+
+    require(kableExtra)
+
+    df %>%
+
+        kable() %>%
+
+        kable_styling(full_width = TRUE, position = 'center') %>%
+
+        scroll_box(height = '200px')
+
+}
